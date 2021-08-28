ec63cb08f9bf8c4eb6317f5edb6ff7c16337574b
Maliur <maliur@protonmail.com>
2021-08-28 19:46:40 +0200

    Add srcery theme

---
+++ lua/lualine/themes/srcery.lua
@@ -0,0 +1,48 @@
+-- Copyright (c) 2020-2021 hoob3rt
+-- MIT license, see LICENSE for more details.
+-- LuaFormatter off
+local colors = {
+  black        = '#1C1B19',
+  white        = '#BAA67F',
+  red          = '#EF2F27',
+  green        = '#519F50',
+  blue         = '#2C78BF',
+  yellow       = '#FBB829',
+  gray         = '#585858',
+  darkgray     = '#303030',
+  lightgray    = '#4E4E4E',
+  inactivegray = '#918175',
+}
+-- LuaFormatter on
+return {
+  normal = {
+    a = {bg = colors.gray, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.lightgray, fg = colors.white},
+    c = {bg = colors.darkgray, fg = colors.gray}
+  },
+  insert = {
+    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.lightgray, fg = colors.white},
+    c = {bg = colors.lightgray, fg = colors.white}
+  },
+  visual = {
+    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.lightgray, fg = colors.white},
+    c = {bg = colors.inactivegray, fg = colors.black}
+  },
+  replace = {
+    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.lightgray, fg = colors.white},
+    c = {bg = colors.black, fg = colors.white}
+  },
+  command = {
+    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    b = {bg = colors.lightgray, fg = colors.white},
+    c = {bg = colors.inactivegray, fg = colors.black}
+  },
+  inactive = {
+    a = {bg = colors.darkgray, fg = colors.gray, gui = 'bold'},
+    b = {bg = colors.darkgray, fg = colors.gray},
+    c = {bg = colors.darkgray, fg = colors.gray}
+  }
+}
